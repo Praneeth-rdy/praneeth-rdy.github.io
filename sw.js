1d087bde601fb7e2ec984516d90bd9a6aaeae944
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-05-25 16:53:06 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c07538f0cfecb57ad9c7.js"
+    "url": "webpack-runtime-f44eb63df281a7aa039e.js"
   },
   {
     "url": "styles.843d69c9d813a51a83c0.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2542f541ef1d8386843dba3d998c29b6"
+    "revision": "873f59f7810b8b44834692edb4ce31a8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-326b917443451985f6dd.js"
